26ad70ada615464fbf423d1440a38a23adb38311
Ganesh J <ganesh.jadhav@toothsi.in>
2021-10-08 15:53:45 +0530

    build added

--- docs/polyfills.js
+++ docs/polyfills.js
@@ -3248,7 +3248,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\gitrepo\ganeshk4.github.io\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/ampa/gitrepo/ganeshk4.github.io/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
